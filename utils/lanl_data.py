af9bd096ee06b8f143d12c3967408b6e9cb2d343
Kyra Wang <cyrus.wang.nian.yu@gmail.com>
2019-04-23 01:30:31 +0000

    Changed to Densenet

--- utils/lanl_data.py
+++ utils/lanl_data.py
@@ -7,19 +7,19 @@ import torch
 import torch.utils.data as data
 
 class LANL_FeatureGenerator(object):
-    def __init__(self, dtype, n_jobs=1, chunk_size=None):
+    def __init__(self, dtype, n_jobs=1, chunk_size=None, train_file='./data/train.csv', test_dir='./data/test/', sample_file='./data/sample_submission.csv'):
         self.chunk_size = chunk_size
         self.dtype = dtype
         self.filename = None
         self.n_jobs = n_jobs
         self.test_files = []
         if self.dtype == 'train':
-            self.filename = './data/train.csv'
+            self.filename = train_file
             self.total_data = int(629145481 / self.chunk_size)
         else:
-            submission = pd.read_csv('./data/sample_submission.csv')
+            submission = pd.read_csv(sample_file)
             for seg_id in submission.seg_id.values:
-                self.test_files.append((seg_id, './data/test/' + seg_id + '.csv'))
+                self.test_files.append((seg_id, test_dir + seg_id + '.csv'))
             self.total_data = int(len(submission))
 
     def read_chunks(self):
@@ -28,7 +28,7 @@ class LANL_FeatureGenerator(object):
                                   dtype={'acoustic_data': np.int16, 'time_to_failure': np.float64})
             for counter, df in enumerate(iter_df):
                 x = df.acoustic_data.values
-                y = df.time_to_failure.values[-1]
+                y = df.time_to_failure.values
                 seg_id = 'train_' + str(counter)
                 yield seg_id, x, y
         else:
@@ -39,8 +39,8 @@ class LANL_FeatureGenerator(object):
 
     def features(self, x, y, seg_id):
         feature_dict = dict()
-        feature_dict['target'] = y
-        feature_dict['segment'] = x
+        feature_dict['target'] = y[-1]
+        feature_dict['segment']
         feature_dict['seg_id'] = seg_id
 
         # create features here
